0fbfd72ae353384ca0a052ce6c77963265e383d0
olkoa003 <43721193+olkoa003@users.noreply.github.com>
2023-01-16 11:24:55 +0000

    added firebase authentication with email and pw

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -9,8 +9,11 @@ export default {
       areas: data.areas
     };
 
+    const token = context.rootGetters.token;
+
     const response = await fetch(
-      `https://coach-app-on-vue-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+      `https://coach-app-on-vue-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json?auth=` +
+        token,
       {
         method: 'PUT',
         body: JSON.stringify(coachData)
